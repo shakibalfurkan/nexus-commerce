c75807d8fe0f8d6c1ba83c0f355b36432f676902
shakibalfurkan <shakibalfurkan@gmail.com>
2026-07-03 17:53:46 +0600

    disconnectPrisma function added

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,20 @@
 import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../../src/generated/prisma/client.js";
+import logger from "../utils/logger.js";
 
 const connectionString = `${process.env.DATABASE_URL}`;
 
 const adapter = new PrismaPg({ connectionString });
 const prisma = new PrismaClient({ adapter });
 
-export { prisma };
+async function disconnectPrisma(): Promise<void> {
+  try {
+    await prisma.$disconnect();
+    logger.info("Prisma disconnected from database.");
+  } catch (err) {
+    logger.error("Error disconnecting Prisma:", err);
+  }
+}
+
+export { prisma, disconnectPrisma };
